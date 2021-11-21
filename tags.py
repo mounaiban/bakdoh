mounaiban/bakdoh

commit 1fe96184e48a0f3abe567dd1379433040477f4a2

Anchor: check DB class in __init__()

tags.py
@@ -52,7 +52,10 @@ class Anchor:
     # Anchor (graph node) class. Includes navigation methods.
     def __init__(self, content, q=None, **kwargs):
         # kwargs accepted: db, do_sync
-        self.db = kwargs.get('db')
+        db = kwargs.get('db')
+        if type(db) is not DB:
+            raise TypeError("cannot use non-TAGS database as db")
+        self.db = db
         self.content = content
         self.q = q
         do_sync = kwargs.get('do_sync', True)

commit 09c4bbc64f19bfa35e5284e292a9844469efe5e8

Anchor: allow DB-less Anchors again
This fixes regression introduced in 1fe9618

tags.py
@@ -53,7 +53,7 @@ class Anchor:
     def __init__(self, content, q=None, **kwargs):
         # kwargs accepted: db, init_sync
         db = kwargs.get('db')
-        if type(db) is not DB:
+        if db is not None and type(db) is not DB:
             raise TypeError("cannot use non-TAGS database as db")
         self.db = db
         self.content = content